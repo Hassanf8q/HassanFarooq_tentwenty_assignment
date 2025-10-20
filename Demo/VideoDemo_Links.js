433b3dfcff553f2fd22269456eb3ae0b0b8808ff
Hassan Farooq <hassan00farooq@gmail.com>
2025-10-20 22:15:06 +0500

    Create Demo Folder and Links of APK | IOS & Android Video Link

---
+++ Demo/VideoDemo_Links.js
@@ -0,0 +1,10 @@
+// Android Video Link 
+// https://drive.google.com/file/d/1rz7Cj59tH4dKaCxQoePMP7yd_sFIshDE/view?usp=drive_link
+
+// IOS video Link 
+// https://drive.google.com/file/d/1g75yx7sBrsLhk_z3NGvPfnnYtnPlwRLx/view?usp=sharing
+
+
+// Android APK 
+// https://drive.google.com/file/d/1t62hg2NUgUMzaRqNSptQVmPOfZtdexW8/view?usp=sharing
+
